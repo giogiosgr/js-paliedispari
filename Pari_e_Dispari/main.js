7b0bec0ebe8fde56ec050318244b7c01b0d742b1
giogiosgr <giovannisgr88@gmail.com>
2024-05-21 19:41:16 +0200

    feat: la funzione random è stata parametrizzata

--- Pari_e_Dispari/main.js
+++ Pari_e_Dispari/main.js
@@ -8,17 +8,17 @@ const digit = parseInt(prompt("scegli un numero da 1 a 5"));
 if (isNaN(digit) || digit < 1 || digit > 5 || (choice.toLowerCase() != "pari" && choice.toLowerCase() != "dispari")) {
     alert("uno o più input non sono corretti");
 }
-//definizione funzione per la generazione e restituzione di un numero random da 1 a 5
-function randomDigit() {
-    return Math.floor(Math.random() * 5) + 1;
+//definizione funzione per la generazione e restituzione di un numero random, con parametri di minimo e massimo
+function randomDigit(min, max) {
+    return Math.floor(Math.random() * (max + 1 - min)) + min;
 }
 //inizializzazione variabile con la somma delle due cifre, richiamando la funzione che genera un numero random
-const sum = digit + randomDigit();
+const sum = digit + randomDigit(1, 5);
 //definzione funzione per valutare se la somma è pari, con restituzione true, altrimenti false
 function isEven(result) {
     return ((result % 2) == 0);
 }
-//condizione di controllo in cui usiamo la funzione che restituisce true opari false, a seconda se la somma sia pari o no
+//condizione di controllo in cui usiamo la funzione che restituisce true oppure false, a seconda se la somma sia pari o no
 //confrontando quindi il risultato con la scelta dell'utente per valutare se ha vinto o meno
 if ((isEven(sum) && choice === "pari") || (!isEven(sum) && choice === "dispari")) {
     alert(`la somma risultante è ${sum}, quindi hai vinto!`);
